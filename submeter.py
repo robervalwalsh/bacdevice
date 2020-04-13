450ce6d961d733859a0a6d62bfc213a50d4e8789
Roberval Walsh <roberval.walsh@gmail.com>
2020-04-13 11:26:46 +0200

    modifications towards live plotting of env sensors

--- submeter.py
+++ submeter.py
@@ -8,6 +8,9 @@ class SubMeter(meter_base.MeterBase):
         self.name = name
         self.is_connected = False
         self.present_value = 0
+        self.present_value = []
+        self.present_date = ""
+        self.section = ""
 
     def start(self):
         try:
@@ -23,3 +26,9 @@ class SubMeter(meter_base.MeterBase):
 
     def getPresentValue(self):
         return self.present_value
+
+    def getPresentDate(self):
+        return self.present_date
+
+    def getSection(self):
+        return self.section
