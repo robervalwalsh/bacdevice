083f8503ec138e9215af38f5d69134d5c7864091
Roberval Walsh <roberval.walsh@desy.de>
2021-10-04 15:22:17 +0200

    another fix username == login

--- desy-weather/recentWeather.py
+++ desy-weather/recentWeather.py
@@ -40,7 +40,7 @@ def combinedWeather(start,end,n):
   
 ##########
 
-df = pd.read_csv('f/home/{username}/daf-monitoring/data/desy-weather/krykWeather.csv',names=("datetime","temperature","pressure","humidity"),parse_dates=[0],infer_datetime_format=True)
+df = pd.read_csv(f'/home/{username}/daf-monitoring/data/desy-weather/krykWeather.csv',names=("datetime","temperature","pressure","humidity"),parse_dates=[0],infer_datetime_format=True)
 
 dt1 = df.iloc[-1]['datetime']
 dt2 = datetime.today()
